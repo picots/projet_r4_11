82b4397961d776853eeae1451d0f687ab0ab9f4b
Arthur POIRIER--DUBOIS <ragnaroukin@gmail.com>
2026-03-20 10:04:04 +0100

    [FEATURE] Add DatabaseHelper

---
+++ app/src/main/java/but/info/projet/utils/DatabaseHelper.kt
@@ -0,0 +1,25 @@
+package but.info.projet.utils
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DatabaseHelper(context: Context) :
+    SQLiteOpenHelper(context, "app.db", null, 1) {
+
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL("""
+            CREATE TABLE club (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                address TEXT,
+                active INTEGER
+            )
+        """)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.execSQL("DROP TABLE IF EXISTS club")
+        onCreate(db)
+    }
+}
