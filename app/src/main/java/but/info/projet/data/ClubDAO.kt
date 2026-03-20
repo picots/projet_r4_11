265b18335e7ec7c547ee5c6ac11cf364f03e9b8b
Arthur POIRIER--DUBOIS <ragnaroukin@gmail.com>
2026-03-20 10:10:54 +0100

    [FEATURE] getAll functions

--- app/src/main/java/but/info/projet/data/ClubDAO.kt
+++ app/src/main/java/but/info/projet/data/ClubDAO.kt
@@ -1,7 +1,58 @@
 package but.info.projet.data
 
-class ClubDao {
-    fun getAll(): List<Club> {}
-    fun insertAll(clubs: List<Club>) {}
-    fun update(club: Club) {}
+import but.info.projet.utils.DatabaseHelper
+
+
+class ClubDao(private val dbHelper: DatabaseHelper) {
+    fun getAll(): List<Club> {
+        val db = dbHelper.readableDatabase
+        val clubs = mutableListOf<Club>()
+
+        val cursor = db.rawQuery("SELECT * FROM club", null)
+
+        if (cursor.moveToFirst()) {
+            do {
+                val club = Club(
+                    id = cursor.getLong(cursor.getColumnIndexOrThrow("id")),
+                    name = cursor.getString(cursor.getColumnIndexOrThrow("name")),
+                    address = cursor.getString(cursor.getColumnIndexOrThrow("address")),
+                    active = cursor.getInt(cursor.getColumnIndexOrThrow("active"))
+                )
+                clubs.add(club)
+            } while (cursor.moveToNext())
+        }
+
+        cursor.close()
+        return clubs
+    }
+
+    fun getAllActive(): List<Club> {
+        val db = dbHelper.readableDatabase
+        val clubs = mutableListOf<Club>()
+
+        val cursor = db.rawQuery("SELECT * FROM club WHERE active=1", null)
+
+        if (cursor.moveToFirst()) {
+            do {
+                val club = Club(
+                    id = cursor.getLong(cursor.getColumnIndexOrThrow("id")),
+                    name = cursor.getString(cursor.getColumnIndexOrThrow("name")),
+                    address = cursor.getString(cursor.getColumnIndexOrThrow("address")),
+                    active = cursor.getInt(cursor.getColumnIndexOrThrow("active"))
+                )
+                clubs.add(club)
+            } while (cursor.moveToNext())
+        }
+
+        cursor.close()
+        return clubs
+    }
+
+    fun insertAll(clubs: List<Club>) {
+
+    }
+
+    fun update(club: Club) {
+
+    }
 }
