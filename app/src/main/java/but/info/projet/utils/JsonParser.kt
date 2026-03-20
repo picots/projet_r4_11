9388b49d821f7a5fd06bedc95816dd965dae057e
picots <solal.picot@gmail.com>
2026-03-20 11:35:15 +0100

    [FEATURE] parse json

--- app/src/main/java/but/info/projet/utils/JsonParser.kt
+++ app/src/main/java/but/info/projet/utils/JsonParser.kt
@@ -1,4 +1,26 @@
 package but.info.projet.utils
 
+import androidx.collection.emptyObjectList
+import but.info.projet.data.Club
+import org.json.JSONArray
+import org.json.JSONObject
+
 class JsonParser {
+    fun parseClubs(json: String): List<Club> {
+        val array = JSONArray(json)
+        var obj: JSONObject
+        var club: Club
+        val clubs: MutableList<Club> = mutableListOf<Club>()
+        for (i in 0..array.length()) {
+            obj = array.getJSONObject(i)
+            club = Club(
+                id = obj.getLong("id"),
+                name = obj.getString("name"),
+                address = obj.getString("address"),
+                active = obj.getInt("active")
+            )
+            clubs.plus(club)
+        }
+        return clubs
+    }
 }
