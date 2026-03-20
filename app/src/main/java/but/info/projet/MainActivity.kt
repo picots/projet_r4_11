6de4e5148452eb6d4f61ead543d5ae8c017dbb04
picots <solal.picot@gmail.com>
2026-03-20 08:30:41 +0100

    [BUILD] create project

---
+++ app/src/main/java/but/info/projet/MainActivity.kt
@@ -0,0 +1,42 @@
+package but.info.projet
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import but.info.projet.ui.theme.ProjetTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ProjetTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    App(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun App(modifier: Modifier = Modifier) {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AppPreview() {
+    ProjetTheme {
+        App()
+    }
+}
