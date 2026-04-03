ddcd2de5585619bc2c6c7c436b33e4ca4f49cc6b
picots <solal.picot@gmail.com>
2026-04-03 12:52:06 +0200

    [FIX] add to club list in JsonParser

--- app/src/main/java/but/info/projet/utils/JsonParser.kt
+++ app/src/main/java/but/info/projet/utils/JsonParser.kt
@@ -1,5 +1,6 @@
 package but.info.projet.utils
 
+import android.util.Log
 import androidx.collection.emptyObjectList
 import but.info.projet.data.Club
 import org.json.JSONArray
@@ -13,15 +14,16 @@ class JsonParser {
         var obj: JSONObject
         var club: Club
         val clubs: MutableList<Club> = mutableListOf()
-        for (i in 0..array.length()) {
+        for (i in 0..<array.length()) {
             obj = array.getJSONObject(i)
             club = Club(
-                id = obj.getLong("id"),
-                name = obj.getString("name"),
-                address = obj.getString("address"),
-                active = obj.getInt("active")
+                id = obj.getLong("club_id"),
+                name = obj.getString("club_name"),
+                address = obj.getString("club_address"),
+                active = obj.getInt("club_active")
             )
-            clubs.plus(club)
+            Log.e("club", club.toString())
+            clubs.add(club)
         }
         return clubs
     }
